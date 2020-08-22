9da7df5ce6cf1c997998f13aa096b1321d0c2318
Rodion <rodionlim@korupartners.com>
2020-08-22 15:51:46 +0800

    First commit

---
+++ StockScraper/StockScraper/AddIn.cs
@@ -0,0 +1,43 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.Registration;
+using System;
+using System.Linq.Expressions;
+
+namespace StockScraper
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            RegisterFunctions();
+        }
+
+        public void AutoClose()
+        {
+        }
+
+        public void RegisterFunctions()
+        {
+            var postAsyncReturnConfig = GetPostAsyncReturnConversionConfig();
+            ExcelRegistration.GetExcelFunctions()
+                .ProcessParameterConversions(postAsyncReturnConfig)
+                .ProcessAsyncRegistrations(nativeAsyncIfAvailable: true)
+                .RegisterFunctions();
+        }
+
+        static ParameterConversionConfiguration GetPostAsyncReturnConversionConfig()
+        {
+            // This conversion replaces the default #N/A return value of async functions with the #GETTING_DATA value.
+            var rval = ExcelError.ExcelErrorGettingData;
+            return new ParameterConversionConfiguration()
+                .AddReturnConversion((type, customAttributes) => type != typeof(object) ? null : ((Expression<Func<object, object>>)
+                                                ((object returnValue) => returnValue.Equals(ExcelError.ExcelErrorNA) ? rval : returnValue)));
+        }
+
+        public ExcelFunctionRegistration UpdateHelpTopic(ExcelFunctionRegistration funcReg)
+        {
+            funcReg.FunctionAttribute.HelpTopic = "http://www.bing.com";
+            return funcReg;
+        }
+    }
+}
---
+++ StockScraper/StockScraper/Scraper.cs
@@ -0,0 +1,43 @@
+﻿using AngleSharp;
+using AngleSharp.Text;
+using ExcelDna.Integration;
+using ExcelDna.Registration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockScraper
+{
+    public static class Scraper
+	{
+		[ExcelAsyncFunction(Description = "Grab latest closing price of a single ticker")]
+		public static async Task<double> bdp(
+			[ExcelArgument(Name = "ticker", Description = "Yahoo finance ticker. Example: \"es3.si\"")] string ticker
+		)
+		{
+			var quoteSummary = await GetHTML(ticker);
+			return quoteSummary["Previous close"].ToDouble();
+		}
+
+		private static async Task<Dictionary<string, string>> GetHTML(string ticker)
+		{
+			var quoteSummary = new Dictionary<string, string>();
+			string keyCache = "";
+			var config = Configuration.Default.WithDefaultLoader();
+			var context = BrowsingContext.New(config);
+			var document = await context.OpenAsync($"https://sg.finance.yahoo.com/quote/{ticker}/");
+
+			var quoteSummaryListItems = document.QuerySelectorAll("div[id='quote-summary'] td");
+
+			foreach (var item in quoteSummaryListItems.Select((sli, i) => new { Value = sli, Index = i }))
+			{
+				if (item.Index % 2 == 0)
+					keyCache = item.Value.TextContent;
+				else
+					quoteSummary.Add(keyCache, item.Value.TextContent);
+			}
+
+			return quoteSummary;
+		}
+	}
+}
