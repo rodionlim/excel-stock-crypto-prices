260f68d6431623b6145edfcc20bbd8672bf1b1ad
Rodion <rodion.lim@hotmail.com>
2020-12-25 10:12:21 +0800

    Adding in support for google finance current price

--- StockScraper/StockScraper/Scraper.cs
+++ StockScraper/StockScraper/Scraper.cs
@@ -1,15 +1,17 @@
 ﻿using AngleSharp;
+using AngleSharp.Io;
 using ExcelDna.Integration;
 using ExcelDna.Registration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace StockScraper
 {
-    public class Scraper
+	public class Scraper
 	{
-		private static readonly List<string> AvailableFields = new List<string>() 
+		private static readonly List<string> AvailableFields = new List<string>()
 		{
 			"PREVIOUS CLOSE",
 			"CURRENT PRICE",
@@ -30,17 +32,30 @@ namespace StockScraper
 			"INCEPTION DATE"
 		};
 
-		[ExcelAsyncFunction(Description = "Grab latest market data of a single exchange traded ticker, such as the closing price, dividend yield etc.")]
+		[ExcelAsyncFunction(Description = "Grab latest Yahoo/Google finance market data of a single exchange traded ticker, such as the closing price, dividend yield etc., defaults to Yahoo finance.")]
 		public static async Task<object> bdp(
-			[ExcelArgument(Name = "ticker", Description = "Yahoo finance ticker. Example: \"es3.si\"")] string ticker,
-			[ExcelArgument(AllowReference = true, Name = "[field]", Description = "[Optional] Yahoo finance field. Example: \"previous close\", \"yield\"")] string field
+			[ExcelArgument(Name = "ticker", Description = "Yahoo/Google finance ticker. Example: \"es3.si\", \"goog\"")] string ticker,
+			[ExcelArgument(AllowReference = true, Name = "[field]", Description = "[Optional] Yahoo finance field. Example: \"previous close\", \"yield\"")] string field,
+			[ExcelArgument(AllowReference = true, Name = "[source]", Description = "[Optional] Specify the Data Source, defaults to yahoo. Example: \"yahoo\", \"google\"")] string source
+
 		)
 		{
+            var defaultField = "CURRENT PRICE";
+
 			field = field.ToUpper();
-			if (field == "") field = "PREVIOUS CLOSE";
+			if (field.Length == 0) field = defaultField;
 			if (!AvailableFields.Contains(field)) return "#Invalid field"; // Validate user input
 
-			var quoteSummary = await GetHTML(ticker);
+			if (source.Length==0) source = "yahoo";
+			if (!(source == "yahoo" | source == "google")) return "#Invalid data source"; // Validate user input
+
+			if (source == "google" & field != defaultField) return "#Google finance currently only supports current price"; // Validate user input
+
+			Dictionary<string, string> quoteSummary;
+			if (source == "yahoo")
+				quoteSummary = await GetHTML(ticker);
+			else
+				quoteSummary = await GetHTMLGoogle(ticker);
 
 			if (double.TryParse(quoteSummary[field], out double res))
 			{
@@ -80,6 +95,22 @@ namespace StockScraper
 				}
 			}
 
+			return quoteSummary;
+		}
+
+		private static async Task<Dictionary<string, string>> GetHTMLGoogle(string ticker)
+		{
+			var quoteSummary = new Dictionary<string, string>();
+			var requester = new DefaultHttpRequester();
+			requester.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+			var config = Configuration.Default.With(requester).WithDefaultLoader();
+			var context = BrowsingContext.New(config);
+			var document = await context.OpenAsync($"https://www.google.com/search?q={ticker}");
+
+			var quoteSummaryListItems = document.QuerySelector("span[jsname='vWLAgc']");
+
+			var tmp = quoteSummaryListItems.TextContent.Replace(",", "");
+			quoteSummary.Add("CURRENT PRICE", tmp);
 			return quoteSummary;
 		}
 	}
